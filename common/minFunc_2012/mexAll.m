05a865d54628faf93377d0d5a6bc925fe45d3791
Enrique Garcia Perez <enrique.garcia.perez@sap.com>
2016-12-30 17:54:43 +0100

    Solving part one and re-compiling mexAll files

--- common/minFunc_2012/mexAll.m
+++ common/minFunc_2012/mexAll.m
@@ -1,7 +1,6 @@
 % minFunc
 fprintf('Compiling minFunc files...\n');
-mex -outdir minFunc/compiled minFunc/mex/mcholC.c
-mex -outdir minFunc/compiled minFunc/mex/lbfgsC.c
-mex -outdir minFunc/compiled minFunc/mex/lbfgsAddC.c
-mex -outdir minFunc/compiled minFunc/mex/lbfgsProdC.c
-
+mex -o minFunc/compiled/mcholC.mex minFunc/mex/mcholC.c
+mex -o minFunc/compiled/lbfgsC.mex minFunc/mex/lbfgsC.c
+mex -o minFunc/compiled/lbfgsAddC.mex minFunc/mex/lbfgsAddC.c
+mex -o minFunc/compiled/lbfgsProdC.mex minFunc/mex/lbfgsProdC.c
