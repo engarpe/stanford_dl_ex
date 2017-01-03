69fcf063ff92f02dcd622455b6d1777de6624ea7
Enrique Garcia Perez <enrique.garcia.perez@sap.com>
2017-01-03 17:24:18 +0100

    Fixing parenthesis in logistic regression solution

--- ex1/logistic_regression.m
+++ ex1/logistic_regression.m
@@ -24,6 +24,6 @@ function [f,g] = logistic_regression(theta, X,y)
 %%% YOUR CODE HERE %%%
 
 for i = 1 : m
-  f = f - y(i) * log( sigmoid(theta' * X(:,i)) )  + ( 1 - y(i)) * log(1 - sigmoid(theta' * X(:,i)));
+  f = f - (y(i) * log( sigmoid(theta' * X(:,i)) )  + ( 1 - y(i)) * log(1 - sigmoid(theta' * X(:,i))));
   g = g + (X(:,i) * ( sigmoid(theta' * X(:,i)) - y(i)));
 endfor
